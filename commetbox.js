bcf1d3f090638f76302933552e616027d51ba8f9
Camila Fonseca <camila@octo.is>
2020-05-06 16:34:09 -0400

    add signup button

--- commetbox.js
+++ commetbox.js
@@ -5,35 +5,4 @@
 // const commentBox = require("commentbox.io");
 // // or if using the CDN, it will be available as a global "commentBox" variable.
 
-// commentBox("5730776725323776-proj");
-
-commentBox("5730776725323776-proj", {
-    className: 'commentbox', // the class of divs to look for
-    defaultBoxId: 'commentbox', // the default ID to associate to the div
-    tlcParam: 'tlc', // used for identifying links to comments on your page
-    backgroundColor: null, // default transparent
-    textColor: null, // default black
-    subtextColor: null, // default grey
-    /**
-     * Creates a unique URL to each box on your page.
-     * 
-     * @param {string} boxId
-     * @param {Location} pageLocation - a copy of the current window.location
-     * @returns {string}
-     */
-    createBoxUrl(boxId, pageLocation) {
-
-        pageLocation.search = ''; // removes query string!
-        pageLocation.hash = boxId; // creates link to this specific Comment Box on your page
-        return pageLocation.href; // return url string
-    },
-    /**
-     * Fires once the plugin loads its comments.
-     * May fire multiple times in its lifetime.
-     * 
-     * @param {number} count
-     */
-    onCommentCount(count) {
-
-    }
-});
+commentBox("5730776725323776-proj");
